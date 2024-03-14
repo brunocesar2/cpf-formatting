742d47c495f3288accf778ce9a3c71604e8adcc6
Bruno César <brunocesar.tenorio1@gmail.com>
2024-03-14 16:32:43 -0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-import ValidarCpf from "./modules/validar-cpf.js";
-const cpf = document.getElementById("cpf");
-const validarCpf = new ValidarCpf(cpf).init();
+const cpfs = document.querySelectorAll(".cpf li");
+const cpfsValues = ([...cpf]) => {
+  return cpf.map((cpf) => {
+    return cpf.innerText;
+  });
+};
+const clearCPF = (cpf) => {
+  return cpf.replace(/\D/g, "");
+};
+const buildCPF = (cpf) => {
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "$1.$2.$3-$4");
+};
+const formatCPF = (cpf) => {
+  return cpf.map(clearCPF).map(buildCPF);
+};
+const changeCPF = (cpfs) => {
+  const cpfsList = cpfsValues(cpfs);
+  const formattedCpfs = formatCPF(cpfsList);
+  cpfs.forEach((cpf, index) => {
+    cpf.innerText = formattedCpfs[index];
+  });
+};
+changeCPF(cpfs);
