9e92cb9a25ae5251501e6463c90a27915008c8b8
Bruno César <brunocesar.tenorio1@gmail.com>
2024-03-14 09:57:59 -0300

    updating js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,23 @@
-const cpfsList = document.querySelectorAll(".cpf li");
-
-const elementsInnerText = ([...elements]) => {
-  return elements.map((element) => {
-    return element.innerText;
+const cpfs = document.querySelectorAll(".cpf li");
+const cpfsValues = ([...cpf]) => {
+  return cpf.map((cpf) => {
+    return cpf.innerText;
   });
 };
 const clearCPF = (cpf) => {
-  return cpf.replace(/[\D]/g, "");
+  return cpf.replace(/\D/g, "");
 };
 const buildCPF = (cpf) => {
   return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "$1.$2.$3-$4");
 };
-const formatCPF = (cpfs) => {
-  return cpfs.map(clearCPF).map(buildCPF);
+const formatCPF = (cpf) => {
+  return cpf.map(clearCPF).map(buildCPF);
 };
-const changeCPF = (cpfsElements) => {
-  const cpfs = elementsInnerText(cpfsElements);
-  const formattedCpfs = formatCPF(cpfs);
-  cpfsElements.forEach((element, index) => {
-    console.log(element);
-    element.innerText = formattedCpfs[index];
+const changeCPF = (cpfs) => {
+  const cpfsList = cpfsValues(cpfs);
+  const formattedCpfs = formatCPF(cpfsList);
+  cpfs.forEach((cpf, index) => {
+    cpf.innerText = formattedCpfs[index];
   });
-  console.log(cpfs);
-  console.log(formattedCpfs);
 };
-changeCPF(cpfsList);
-console.log(cpfsList);
-console.log(elementsInnerText(cpfsList));
+changeCPF(cpfs);
