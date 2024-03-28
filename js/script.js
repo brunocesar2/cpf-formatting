747704b49466cde1e52bfa8ef51e78af779ab9ad
Bruno César <brunocesar.tenorio1@gmail.com>
2024-03-28 13:06:16 -0300

    updating js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 const cpfs = document.querySelectorAll(".cpf li");
-const cpfsArray = ([...cpf]) => {
-  return cpf.map((cpf) => {
+const cpfsValues = ([...cpfs]) => {
+  return cpfs.map((cpf) => {
     return cpf.innerText;
   });
 };
 const clear = (cpf) => {
-  return cpf.replace(/\D/g, "");
+  return cpf.replace(/\D/gi, "");
 };
 const build = (cpf) => {
-  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "$1.$2.$3-$4");
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/gi, "$1.$2.$3-$4");
 };
 const format = (cpf) => {
   return cpf.map(clear).map(build);
 };
-const change = (cpfs) => {
-  const cpfsValues = cpfsArray(cpfs);
-  const formattedCpfs = format(cpfsValues);
+const change = (cpfFormatted) => {
   cpfs.forEach((cpf, index) => {
-    cpf.innerText = formattedCpfs[index];
+    cpf.innerText = cpfFormatted[index];
   });
 };
-change(cpfs);
+change(format(cpfsValues(cpfs)));
